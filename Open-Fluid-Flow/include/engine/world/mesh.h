9c289cdf478cb084dd55904ac79597e357000aab
William Colin Peters <wcpeters93@gmail.com>
2016-05-05 03:31:14 -0400

    Working on meshes

--- Open-Fluid-Flow/include/engine/world/mesh.h
+++ Open-Fluid-Flow/include/engine/world/mesh.h
@@ -1,10 +1,10 @@
 // Dense Matrix eventually make this generic
 #ifndef MESH_H
 #define MESH_H
-#include <string.h>
-#include <iostream>
+#include "math/mat.h"
 
-// represents a single dimensional mesh
+
+// represents a single dimensional mesh (2D solution)
 struct Mesh1D
 {
 private:
@@ -15,13 +15,14 @@ public:
   Mesh1D(size, dx);
 };
 
-// represents a two dimensional mesh
+// represents a two dimensional mesh (3D solution)
 struct Mesh2D
 {
-
+  Mat* mesh;
+  
 };
 
-// represents a three dimensional mesh
+// represents a three dimensional mesh (this would give a 4D solution)
 struct Mesh3D
 {
 
