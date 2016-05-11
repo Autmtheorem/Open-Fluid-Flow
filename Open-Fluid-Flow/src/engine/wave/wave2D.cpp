c682221bc7703013b781e634dd1fe47789a3d95a
William Colin Peters <wcpeters93@gmail.com>
2016-05-11 00:00:17 -0400

    Reworking integrate function

--- Open-Fluid-Flow/src/engine/wave/wave2D.cpp
+++ Open-Fluid-Flow/src/engine/wave/wave2D.cpp
@@ -11,13 +11,15 @@ Wave2D::Wave2D(float dt, float speed, float decay)
 // integeration function (finite difference method, how to handle others??)
 Mesh2D Wave2D::integrate(Mesh2D mesh)
 {
-  // pre-compute values
-  //float q = 2-(this->dt * this->decay);
-  //float r = -1 + (this->decay * this->dt);
-  //float b = ((c*c)*(this->dt * this->dt))/(mesh->dx * mesh->dx);
+  // pre-compute constants
+  float calc_1 = (2-(this->decay*this->dt)); // constant for first term
+  float calc_2 = (-1 + (this->decay*this->dt)); // constant for second term
+  float calc_3 = ((this->speed*this->speed)*(this->dt*this->dt))/(mesh.dx); // constant for third term
+
+
 }
 
-// calculates the convolution between two matricies (should I use this??)
+// calculates the convolution between two matricies (move to math lib??)
 Mat Wave2D::convolution(Mat A, Mat B)
 {
 
